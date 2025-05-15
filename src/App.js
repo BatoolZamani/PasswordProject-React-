a30cd925f5e688d4785fb3ea9c81edd91a69129d
b.zamani@renolos.com <b.zamani@renolos.com>
2025-05-15 11:38:35 +0200

    done project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import {FaEye} from "react-icons/fa";
+import React, { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
 function App() {
+  const [type, setType] = useState("password");
+  const [icon, setIcon] = useState(<FaEyeSlash />);
+
+  const handleClick = () => {
+    if (type === "password") {
+      setType("text");
+      setIcon(<FaEye />);
+    } else {
+      setType("password");
+      setIcon(<FaEyeSlash />);
+    }
+  };
+
   return (
     <div className="wrapper">
       <div className="input-fields">
-      <input type="password" />
-      <span><FaEye/></span>
+        <input type={type} placeholder="Enter password" />
+        <span onClick={handleClick}>{icon}</span>
       </div>
-   
     </div>
   );
 }
